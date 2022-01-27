ce346ae87ff16fb6a42a783d9994bba07ac50abb
SunCreamGames <tg : @Sun_Cream>
2022-01-27 16:40:16 +0200

    Added signals

--- Obrio/Assets/Scripts/Infrastructure/MainInstaller.cs
+++ Obrio/Assets/Scripts/Infrastructure/MainInstaller.cs
@@ -1,3 +1,4 @@
+using Signals;
 using UnityEngine;
 using Zenject;
 
@@ -5,5 +6,16 @@ public class MainInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        SignalBusInstaller.Install(Container);
+
+        DeclareSignals();
+    }
+
+    private void DeclareSignals()
+    {
+        Container.DeclareSignal<CollisionWithObstacle>();
+        Container.DeclareSignal<PickUpCoin>();
+        Container.DeclareSignal<LevelFailing>();
+        Container.DeclareSignal<LevelStarting>();
     }
 }
---
+++ Obrio/Assets/Scripts/Signals/Signals.cs
@@ -0,0 +1,20 @@
+using Zenject;
+
+namespace Signals
+{
+    public class CollisionWithObstacle
+    {
+    }
+
+    public class PickUpCoin
+    {
+    }
+
+    public class LevelStarting
+    {
+    }
+
+    public class LevelFailing
+    {
+    }
+}
